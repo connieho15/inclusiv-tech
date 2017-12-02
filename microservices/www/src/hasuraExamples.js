07a75d2b155cf75e949de73518c35e60889d4f17
Connie Ho <Ho.Connie10@gmail.com>
2017-12-02 14:54:37 -0800

    Final

--- microservices/www/src/hasuraExamples.js
+++ microservices/www/src/hasuraExamples.js
@@ -13,4 +13,33 @@ router.route("/alex-text").post(function (req, res) {
     res.json(alex(req.body.text).messages)
 })
 
+router.route("/alex-text-replace").post(function (req, res){
+  toReplace = "";
+  replace = "";
+  output = req.body.text;
+  errors = alex(req.body.text).messages
+   for (i = 0; i < errors.length; i++){
+       console.log(errors[i].message)
+        if ((errors[i].message).includes("may be insensitive")){
+          toReplace = replaceParse(errors[i].message)[0]
+          replace = replaceParse(errors[i].message)[1]
+          output = output.replace(toReplace, replace)
+      }
+      if ((errors[i].message).includes("it's profane")){
+
+      }
+    }
+  res.header("Access-Control-Allow-Origin", "*");
+  res.json([{
+    "replacement_string" : output
+  }])
+})
+
+function replaceParse(str) {
+  str = str.replace(/`/g, '');
+  var res = str.split(" may be insensitive, use ")[0];
+  var toReplace = str.split(" may be insensitive, use ")[1].split(",")[0].replace(" instead", "");
+  return [res, toReplace];
+}
+
 module.exports = router;
