ae138e09db80d0b09b1da321998ea13f9810e45b
Connie Ho <Ho.Connie10@gmail.com>
2020-07-01 16:29:28 -0700

    Whats on my mac

--- microservices/www/src/hasuraExamples.js
+++ microservices/www/src/hasuraExamples.js
@@ -3,6 +3,11 @@ var router = express.Router();
 var config = require('./config');
 var request = require('request');
 var alex = require('alex')
+var Dictionary = require("oxford-dictionary-api");
+var app_id = "89386bcd"; 
+var app_key = "cd6a4fc2437aa2b93b47c24ca32de8c7";
+
+var dict = new Dictionary(app_id,app_key);
 
 router.route("/").get(function (req, res) {
   res.render('./static/index.html');
@@ -14,32 +19,53 @@ router.route("/alex-text").post(function (req, res) {
 })
 
 router.route("/alex-text-replace").post(function (req, res){
-  toReplace = "";
-  replace = "";
+  var replacements = {};
+  replaced = "";
+  replacement = "";
   output = req.body.text;
+  errorMsgs = []
   errors = alex(req.body.text).messages
    for (i = 0; i < errors.length; i++){
+       errorMsgs.push(errors[i].message)
        console.log(errors[i].message)
         if ((errors[i].message).includes("may be insensitive")){
-          toReplace = replaceParse(errors[i].message)[0]
-          replace = replaceParse(errors[i].message)[1]
-          output = output.replace(toReplace, replace)
-      }
-      if ((errors[i].message).includes("it's profane")){
-
+          replaced = replaceParse(errors[i].message)[0]
+          console.log(replaced)
+          replacement = replaceParse(errors[i].message)[1]
+          console.log(replacement)
+          output = output.replace(replaced, replacement)
+          console.log(output)
+          replacements[replaced] = replacement
+      } else {
+      //   if ((errors[i].message).includes("it's profane")){
+      //     var profanity = errors[i].message.replace("Don’t use", "").replace(", it’s profane", "").replace(/"/g, "").trim()
+      //     console.log(profanity)
+      //     dict.find(profanity, function(error,data){ 
+      //       if(error){
+      //         return console.log(error);
+      //       } else {
+      //       console.log(data); 
+      //       }
+      //     });          
+      //   }
       }
     }
   res.header("Access-Control-Allow-Origin", "*");
   res.json([{
-    "replacement_string" : output
+    "replacement_string" : output,
+    "replacements" : replacements,
+    "comments" : errorMsgs
+
   }])
 })
 
+
+
 function replaceParse(str) {
   str = str.replace(/`/g, '');
-  var res = str.split(" may be insensitive, use ")[0];
-  var toReplace = str.split(" may be insensitive, use ")[1].split(",")[0].replace(" instead", "");
-  return [res, toReplace];
+  var replaced = str.split(" may be insensitive, use ")[0];
+  var replacement = str.split(" may be insensitive, use ")[1].split(",")[0].replace(" instead", "");
+  return [replaced, replacement];
 }
 
 module.exports = router;
